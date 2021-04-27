cf107363550c8897c9a737388b7fcf9be8f942f2
Lévai Viktória <levaiviktoriaanna@gmail.com>
2021-04-27 17:45:28 +0200

    3. Activity, adatok kiírása

--- app/src/main/java/com/example/oltsareg/ThirdActivity.java
+++ app/src/main/java/com/example/oltsareg/ThirdActivity.java
@@ -3,6 +3,7 @@ package com.example.oltsareg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+        TextView sikeres = findViewById(R.id.sikeres);
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null){
+            sikeres.setText(("Sikeres regisztráció!")+"\n" + (extras.getString("adottnev"))+"\n"+ (extras.getString("adottszuldat"))+"\n"+(extras.getString("adottszigszam")));
+        }
     }
+
 }
